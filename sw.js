69f0f84bcf9ad8299d5def65e62939f03acb81cb
csiveyer-create <c.siveyer@gmail.com>
2026-07-30 10:53:22 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "allstunts-assets-v4";
+const FILES = [
+  "./",
+  "./index.html",
+  "./css/styles.css?v=20260730-4",
+  "./js/app.js?v=20260730-4",
+  "./data/inventory-data.js?v=20260730-4",
+  "./assets/logo.svg?v=20260730-4",
+  "./manifest.webmanifest?v=20260730-4"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(hit => hit || caches.match("./index.html")))
+  );
+});
